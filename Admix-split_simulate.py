7b6e79bd1ce8f2aee65bd3922d4c320b6a4a8563
samslamowicz <46546270+samslamowicz@users.noreply.github.com>
2020-10-16 12:26:03 +1100

    Add files via upload

--- Admix-split_simulate.py
+++ Admix-split_simulate.py
@@ -7,7 +7,7 @@ from numpy import savetxt
 
 print('test')
 def ts_sim(t_a,t_as,alpha):
-    t_s = 100
+    t_s = 200
     N1 = 200
     pop_configs = [
         msprime.PopulationConfiguration(sample_size=50, growth_rate = 0,initial_size=N1),
@@ -31,23 +31,24 @@ def ts_sim(t_a,t_as,alpha):
 
 def generate_ancestry_table(ts,samples):
     census_ancestors = list(compress(list(range(ts.num_nodes)), ts.tables.nodes.flags==1048576))
-    ancestry_table = np.array(ts.tables.link_ancestors(samples=samples,ancestors=census_ancestors))
+    ancestry_table = ts.tables.link_ancestors(samples=samples,ancestors=census_ancestors)
     return ancestry_table
-def local_ancestry(samples,ancestry_table,pop_id):
+def generate_tracts(samples,ancestry_table,pop_id):
+    ##Outputs a list of list tracts and a list of list of tract ids. Each list corresponds to ancestry of a sample
+    ancestry_table = np.array((ancestry_table.left,ancestry_table.right,ancestry_table.parent,ancestry_table.child)).T
     anc = ancestry_table[ancestry_table[:,0].argsort()]
     num_samples = max(samples) + 1
     diff_pop = False
     tract_pop = [0]*num_samples
-    for j in range(num_samples):
+    for j in range(len(samples)):
         sample = int(anc[j,3])
         tract_pop[sample] = pop_id[int(anc[j,2])]
     tracts = [[] for i in range(num_samples)]
     tract_ids = [[] for i in range(num_samples)]
     tract_start = [0 for i in range(num_samples)]
     sample_index = [0]
-    tract_end = max(anc[:,1])
+    tract_end = int(max(anc[:,1]))
     for k in range(np.shape(anc)[0]):
-        #sample = samples.index(int(anc[k,3]))
         sample = int(anc[k,3])
         current_pop = pop_id[int(anc[k,2])]
         diff_pop = current_pop - tract_pop[sample]
@@ -55,7 +56,7 @@ def local_ancestry(samples,ancestry_table,pop_id):
             tracts[sample].append(anc[k,0]-tract_start[sample])
             tract_ids[sample].append(tract_pop[sample])
             tract_start[sample] = anc[k,0]
-        elif anc[k,1] == tract_end:
+        if anc[k,1] == tract_end:
             tracts[sample].append(anc[k,1]-tract_start[sample])
             tract_ids[sample].append(tract_pop[sample])
         tract_pop[sample] = current_pop
@@ -65,48 +66,51 @@ def local_ancestry(samples,ancestry_table,pop_id):
 #ts - tree sequence
 #sample_pops - population id of samples for which local ancestry statistics are calculated
 #anc_pops = ancestral population ids for which tract lengths are calculated (2 populations)
-def local_ancestry_stats(ts,sample_pops,anc_pops,plot=False,return_l=False):
+def sort_tracts(ts,sample_pops,anc_pops,plot=False):
+    #Calls previous 2 functions and sorts tracts to make a list of lists of tract lengths,
+    #sorted by ancestral population
     samples = []
+    len_anc_pops = len(anc_pops)
     for population in sample_pops:
         samples += list(ts.samples(population=population))
     ancestry_table = generate_ancestry_table(ts,samples)
     pop_id = ts.tables.nodes.population
     anc_ratio = []
-    L = [] #all tract lengths
-    L_0 = []#all tract lengths with ancestry from ancestral population 0
-    L_1 = []#all tract lengths with ancestry from ancestral population 1
+    L_split = [[] for i in anc_pops]
     lowerpop = min(anc_pops)
-    tracts,tract_ids = local_ancestry(samples,ancestry_table,pop_id)
+    tracts,tract_ids = generate_tracts(samples,ancestry_table,pop_id)
     for j in range(len(tracts)):
-        L.extend(tracts[j])
-        l_0 = list(compress(tracts[j],[bool(i) for i in np.array(tract_ids[j])-lowerpop]))
-        l_1 = list(compress(tracts[j],[not bool(i) for i in np.array(tract_ids[j])-lowerpop]))
-        L_0.extend(l_0)
-        L_1.extend(l_1)
-    if L_0:
-        mean_L_0 = np.mean(L_0)
-        var_L_0 = np.var(L_0)
+        for i in range(len_anc_pops):
+            l = list(compress(tracts[j],[not bool(i) for i in np.array(tract_ids[j])-anc_pops[i]]))
+            L_split[i].extend(l)
+    return(L_split)
+def calculate_stats(tracts,total_length,inc_ratio=True):
+    #Calculates statistics on the output of sort_tracts
+    if tracts:
+        stats = [len(tracts),np.mean(tracts),np.var(tracts)]
     else:
-        mean_L_0 = 0
-        var_L_0 = 0
-    if L_1:
-        mean_L_1 = np.mean(L_1)
-        var_L_1 = np.var(L_1)
-    else:
-        mean_L_1 = 0
-        var_L_1 = 0
-    stats = [len(L),len(L_0),len(L_1),np.mean(L),mean_L_0,mean_L_1,np.var(L),var_L_0,var_L_1,sum(L_0)/sum(L)]
-    if plot:
-        bin_list = list(range(0,int(1e8),int(5e5)))
-        plt.hist(L,bins=bin_list,histtype = 'step')
-        plt.hist(L_0,bins=bin_list,histtype = 'step')
-        plt.hist(L_1,bins=bin_list,histtype = 'step')
-        plt.xlim(xmin=0, xmax = 5e7)
-        plt.show()
-    if return_l:
-        return(stats,L,L_0,L_1)
-    else:
-        return(stats)
+        stats = [0,0,0]
+    if inc_ratio:
+        stats.append(sum(tracts)/total_length)
+    return(stats)
+def local_ancestry_stats(ts,sample_pops,anc_pops,plot=False):
+    #calls all functions
+    tracts_list = sort_tracts(ts,sample_pops,anc_pops,plot=plot)
+    stats = []
+    L = [tract for tracts in tracts_list for tract in tracts]
+    total_length = sum(L)
+    stats.extend(calculate_stats(L,total_length,inc_ratio=False))
+    for tracts in tracts_list:
+        stats.extend(calculate_stats(tracts,total_length,inc_ratio=True))
+    return(stats)
+def standard_stats(ts):
+    samples = ts.samples()
+    pops = [ts.samples(population=0),ts.samples(population=1),ts.samples(population=2),ts.samples(population=3)]
+    stats = [ts.f2([pops[3],pops[0]]),ts.f2([pops[3],pops[1]]),ts.f2([pops[3],pops[2]]),
+             ts.Fst([pops[3],pops[0]]),ts.Fst([pops[3],pops[1]]),ts.Fst([pops[3],pops[2]]),
+             ts.f3([pops[3],pops[0],pops[1]]),ts.f3([pops[3],pops[0],pops[2]]),ts.f3([pops[3],pops[1],pops[2]]),
+             ts.diversity(),ts.diversity(pops[3]),ts.Tajimas_D(),ts.segregating_sites()]
+    return stats
 def standard_stats(ts):
     samples = ts.samples()
     pops = [ts.samples(population=0),ts.samples(population=1),ts.samples(population=2),ts.samples(population=3)]
@@ -116,10 +120,10 @@ def standard_stats(ts):
              ts.diversity(),ts.Tajimas_D(),ts.segregating_sites()]
     return stats
 def ABCsimulate(iterations):
-    stats=np.zeros((iterations,39))
+    stats=np.zeros((iterations,41))
     samples=np.zeros((iterations,3))
     for i in range(iterations):
-        t = (np.random.uniform(1,95),np.random.uniform(1,95))
+        t = (np.random.uniform(1,195),np.random.uniform(1,195))
         t_a,t_as = (max(t),min(t))    
         alpha = np.random.uniform(0,1)
         ts = ts_sim(t_a,t_as,alpha)
